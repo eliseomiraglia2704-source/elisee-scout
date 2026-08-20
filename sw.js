39f217182d55c413d43098999f0e86ad49261d9a
Eliseo2704 <eliseomiraglia2704@gmail.com>
2026-08-20 14:17:59 +0200

    Torino Women: scritta del logo in bianco, scudetto granata invariato.

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    - MAI servire HTML al posto di JS/CSS (causa "Unexpected token <")
    - Fallback HTML solo per navigazioni pagina
 */
-const CACHE = 'elisee-scout-v20260820-meda4';
+const CACHE = 'elisee-scout-v20260820-tw1';
 const PRECACHE = [
   '/',
   '/index.html',
