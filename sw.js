a1fd2050fed25e3e72114ab3d3f404e8d0259215
Eliseo2704 <eliseomiraglia2704@gmail.com>
2026-08-30 13:23:46 +0200

    Aggiornato logo ufficiale Carpenedolo BSV Garda da Tuttocampo

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    - MAI servire HTML al posto di JS/CSS (causa "Unexpected token <")
    - Fallback HTML solo per navigazioni pagina
 */
-const CACHE = 'elisee-scout-v20260830-logobre1';
+const CACHE = 'elisee-scout-v20260830-logocarp1';
 const PRECACHE = [
   '/',
   '/index.html',
